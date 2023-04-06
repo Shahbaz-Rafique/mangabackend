19f5525536823f8991eb99359a623a9fba77dab8
Shahbaz-Rafique <97734247+Shahbaz-Rafique@users.noreply.github.com>
2023-04-06 17:55:26 +0500

    commit

--- routes/login.js
+++ routes/login.js
@@ -10,10 +10,10 @@ router.post('/',(req,res,next)=>{
     const hashpass = crypto.createHash('sha256').update(password).digest('hex');
     User.find({emails:email,password:hashpass,emailverify:"verified"}).then((results) => {
         if(results.length==1){
-            res.redirect(`http://localhost:3000/login-home?email=${email}&login=true`);
+            res.redirect(`http://toonvortex.com/login-home?email=${email}&login=true`);
         }
         else if(results.length==0){
-            res.redirect('http://localhost:3000/login?login=false')
+            res.redirect('http://toonvortex.com/login?login=false')
         }
       }).catch((err) => {
         console.log(err);
